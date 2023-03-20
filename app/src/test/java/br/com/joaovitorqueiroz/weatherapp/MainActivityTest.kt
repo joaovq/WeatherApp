98a9712706a49edc690e773caa79db6c0d5b43b4
joaovq <vitorsnta90@gmail.com>
2023-03-20 16:25:41 -0300

    test: delete test is location enabled and refactor

--- app/src/test/java/br/com/joaovitorqueiroz/weatherapp/MainActivityTest.kt
+++ app/src/test/java/br/com/joaovitorqueiroz/weatherapp/MainActivityTest.kt
@@ -3,20 +3,15 @@ package br.com.joaovitorqueiroz.weatherapp
 import android.content.Context
 import android.location.LocationManager
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
+@RunWith(MockitoJUnitRunner::class)
 class MainActivityTest {
     @Mock
     private lateinit var mockContext: Context
 
-    @Test
-    fun `is location enabled test`() {
-        val mockLocationManager = mock<LocationManager> {
-            on {
-                mockContext.getSystemService(Context.LOCATION_SERVICE)
-            } doReturn LocationManager.GPS_PROVIDER
-        }
-    }
 }
